94780988d00869bb955b86feba9eda5c17c24fde
arthurbgrs <arthur.f.borges6@aluno.senai.br>
2026-03-17 17:09:21 -0300

    tcc

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -16,20 +16,25 @@ async function buscarUsuarios() {
   preencherLista(lista);
 }
 
-function preencherLista(lista) {
-  lista.forEach((funcionario) => {
-    const li = document.createElement("li");
-    li.classList.add("funcionario");
-    const h2 = document.createElement("h2");
-    h2.textContent = funcionario.nome;
-    const button = document.createElement("button");
-    button.textContent = "Detalhes";
-    button.addEventListener("click", () => showDetails(funcionario.id));
 
-    li.append(h2, button);
-    ul.appendChild(li);
-  });
+  function preencherLista(lista) {
+  lista
+    .filter(funcionario => funcionario.ativo)
+    .forEach((funcionario) => {
+      const li = document.createElement("li");
+      li.classList.add("funcionario");
+      const h2 = document.createElement("h2");
+      h2.textContent = funcionario.nome;
+      const button = document.createElement("button");
+      button.textContent = "Detalhes";
+      button.addEventListener("click", () => showDetails(funcionario.id));
+
+      li.append(h2, button);
+      ul.appendChild(li);
+    });
 }
+
+
 
 async function showDetails(id) {
   try {
@@ -44,7 +49,6 @@ async function showDetails(id) {
 }
 
 function openModal(data) {
- 
   modalFields.nome.textContent = `Nome: ${data.nome || ""}`;
   modalFields.cpf.textContent = `CPF: ${data.cpf || ""}`;
   modalFields.departamento.textContent = `Departamento: ${data.departamento || ""}`;
@@ -65,3 +69,25 @@ modal.addEventListener("click", (e) => {
 });
 
 buscarUsuarios();
+
+const quantidade = document.querySelector("#quantidade");
+let quantidadeTotal = 0
+
+async function puxarQuantidade() {
+  try {
+    const resposta = await fetch("http://localhost:3000/funcionarios");
+    const lista = await resposta.json();
+  
+    const map = lista.map((funcionario) => {
+      if (funcionario.ativo) {
+        quantidadeTotal++
+      }
+    });
+
+      quantidade.textContent = quantidadeTotal + " Funcionarios"
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+puxarQuantidade()
